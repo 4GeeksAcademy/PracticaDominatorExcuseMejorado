9aed747b0896ecf48ef847a80bfdfc855af4b1cb
angeljimenez91 <139405887+angeljimenez91@users.noreply.github.com>
2023-11-22 20:28:12 +0000

    mejorar

--- src/app.js
+++ src/app.js
@@ -7,16 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-
+  function dominator(p, a, n) {
+    let resultado = [];
+    for (var i = 0; i < p.length; i++) {
+      for (var j = 0; j < a.length; j++) {
+        for (var k = 0; k < n.length; k++) {
+          resultado += " " + p[i] + a[j] + n[k] + ".com";
+        }
+      }
+    }
+    return resultado;
+  }
   let pronoun = ["the", "our"];
   let adj = ["great", "big"];
   let noun = ["jogger", "racoon"];
 
-  for (var i = 0; i < pronoun.length; i++) {
-    for (var j = 0; j < adj.length; j++) {
-      for (var k = 0; k < noun.length; k++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + ".com");
-      }
-    }
-  }
+  let html = dominator(pronoun, adj, noun);
+  document.querySelector("#dominator").innerHTML = html
+    .map(item => '<li class="list-group-item">' + item + "</li>")
+    .join("");
+  console.log(html);
 };
